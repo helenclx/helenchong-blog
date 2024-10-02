e4fefbc2d7d0bae4f11748be0aab30efa5c2a8a1
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-10-03 03:47:14 +0800

    Add site metadata

---
+++ src/_data/sitemeta.js
@@ -0,0 +1,13 @@
+export const siteName = "Helen Chong's Journal'";
+export const siteDomain = "blog.helenchong.omg.lol";
+export const siteUrl = "https://" + siteDomain || "http://localhost:8080";
+export const siteAuthor = {
+	name: "Helen Chong",
+	email: "helenchong@omg.lol",
+	url: siteUrl + "/about"
+};
+export const siteDescription = siteAuthor.name + "'s personal blog.";
+export const siteLang = "en";
+export const siteLocale = "en_MY";
+export const feedPath = "/feed.xml";
+export const feedUrl = siteUrl + feedPath;
