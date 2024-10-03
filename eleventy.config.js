7a2f8cfa387d2debc278a60c868e0333843471a6
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-10-03 23:02:01 +0800

    Pre-process HTML files as Nunjusks template

--- eleventy.config.js
+++ eleventy.config.js
@@ -14,6 +14,7 @@ export default function(eleventyConfig) {
 
     return {
         markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
         dir: {
             input: "src"
         }
