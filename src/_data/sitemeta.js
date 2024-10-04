6e2cf29ce62d650d1cc91630eba76fd8384dd348
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-10-04 18:00:39 +0800

    Use localized about page for site author URL

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -2,13 +2,13 @@ export const siteDomain = "blog.helenchong.omg.lol";
 export const siteUrl = "https://" + siteDomain || "http://localhost:8080";
 export const siteAuthor = {
 	email: "helenchong@omg.lol",
-	url: siteUrl + "/about"
 };
 
 export const en = {
 	siteName: "Helen Chong's Journal",
 	siteAuthor: {
-		name: "Helen Chong"
+		name: "Helen Chong",
+		url: siteUrl + "/en/about"
 	},
 	siteDescription: () => this.siteAuthor.name + "'s personal blog."
 };
@@ -16,7 +16,8 @@ export const en = {
 export const zh = {
 	siteName: "張海倫的日誌",
 	siteAuthor: {
-		name: "張海倫"
+		name: "張海倫",
+		url: siteUrl + "/zh/about"
 	},
 	siteDescription: () => this.siteAuthor.name + "的個人部落格。"
 };
