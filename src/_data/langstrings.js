b89f9804124a33f0b78f266e1f227c50a0976923
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-10-04 17:52:24 +0800

    Rename localized strings data file

---
+++ src/_data/langstrings.js
@@ -0,0 +1,8 @@
+export const en = {
+    dir: '', // stands for the direction of the language set in the head, defaults to LTR (left to right)
+    availableText: 'This page is also available in:'
+};
+
+export const zh = {
+    availableText: '此頁面也有此語言版本：'
+};
--- src/_data/languages.js
+++
@@ -1,9 +0,0 @@
-export default {
-    en: {
-        dir: '', // stands for the direction of the language set in the head, defaults to LTR (left to right)
-        availableText: 'This page is also available in:'
-    },
-    zh: {
-        availableText: '此頁面也有此語言版本：'
-    }
-};
