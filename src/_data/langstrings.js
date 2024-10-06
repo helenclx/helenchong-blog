bb1a7f92ab8bbd480eff009a5357fdb2a590d013
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-10-06 12:11:42 +0800

    Move language options to the header

--- src/_data/langstrings.js
+++ src/_data/langstrings.js
@@ -1,12 +1,16 @@
 export const en = {
     dir: '', // stands for the direction of the language set in the head, defaults to LTR (left to right)
     availableText: 'This page is also available in:',
+    langOptions: 'Language Options:',
+    mainNav: 'Main',
     published: 'Published:',
     author: 'Author:',
 };
 
 export const zh = {
     availableText: '此页面也有此语言版本：',
+    langOptions: '语言选项：',
+    mainNav: '主要',
     published: '发布：',
     author: '作者：',
 };
