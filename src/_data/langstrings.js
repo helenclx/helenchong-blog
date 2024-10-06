84d4b8ca3d0ca610bd570cbe8f1f769a4a6c6886
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-10-06 09:22:58 +0800

    Set up page and post layout templates

--- src/_data/langstrings.js
+++ src/_data/langstrings.js
@@ -1,8 +1,12 @@
 export const en = {
     dir: '', // stands for the direction of the language set in the head, defaults to LTR (left to right)
-    availableText: 'This page is also available in:'
+    availableText: 'This page is also available in:',
+    published: 'Published:',
+    author: 'Author:',
 };
 
 export const zh = {
-    availableText: '此页面也有此语言版本：'
+    availableText: '此页面也有此语言版本：',
+    published: '发布：',
+    author: '作者：',
 };
