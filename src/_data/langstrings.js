0a6ab225bb9ea151f64796309e7dda83abd7a753
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-10-06 13:38:44 +0800

    Rename available language strings

--- src/_data/langstrings.js
+++ src/_data/langstrings.js
@@ -1,6 +1,6 @@
 export const en = {
     dir: '', // stands for the direction of the language set in the head, defaults to LTR (left to right)
-    availableText: 'This page is also available in:',
+    availableLang: 'This page is also available in:',
     langOptions: 'Language Options:',
     mainNav: 'Main',
     published: 'Published:',
@@ -8,7 +8,7 @@ export const en = {
 };
 
 export const zh = {
-    availableText: '此页面也有此语言版本：',
+    availableLang: '此页面也有此语言版本：',
     langOptions: '语言选项：',
     mainNav: '主要',
     published: '发布：',
