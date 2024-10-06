7ecf71685fe6b9d839aa4cf6e3816c31dd3aee1c
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-10-06 17:57:57 +0800

    Add navigation header and footer arrays

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,25 +1,35 @@
-export const en = [
-    {
-        text: "Home",
-        url: "/en/"
-    },
-    {
-        text: "About",
-        url: "/en/about/"
-    },
-    {
-        text: "Posts",
-        url: "/en/posts/"
-    }
-];
+export const en = {
+    header: [
+        {
+            text: "Home",
+            url: "/en/"
+        },
+        {
+            text: "About",
+            url: "/en/about/"
+        },
+        {
+            text: "Posts",
+            url: "/en/posts/"
+        }
+    ],
+    footer: []
+};
 
-export const zh = [
-    {
-        text: "首页",
-        url: "/zh/"
-    },
-    {
-        text: "文章",
-        url: "/zh/posts/"
-    }
-];
+export const zh = {
+    header: [
+        {
+            text: "首页",
+            url: "/zh/"
+        },
+        {
+            text: "关于",
+            url: "/zh/about/"
+        },
+        {
+            text: "文章",
+            url: "/zh/posts/"
+        }
+    ],
+    footer: []
+};
