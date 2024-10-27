86ebb6d077fcf41abb900b123d40e86427ce75f6
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-10-27 19:42:00 +0800

    Remove date from post URLs

--- src/en/posts/posts.11tydata.js
+++ src/en/posts/posts.11tydata.js
@@ -1,4 +1,7 @@
 export default {
     layout: "layouts/post",
-    tags: ["posts", "posts_en"]
+    tags: ["posts", "posts_en"],
+    eleventyComputed: {
+        permalink: "/{{ locale }}/posts/{{ page.fileSlug }}/"
+    }
 }
--- src/zh/posts/posts.11tydata.js
+++ src/zh/posts/posts.11tydata.js
@@ -1,4 +1,7 @@
 export default {
     layout: "layouts/post",
-    tags: ["posts_zh"]
+    tags: ["posts_zh"],
+    eleventyComputed: {
+        permalink: "/{{ locale }}/posts/{{ page.fileSlug }}/"
+    }
 }
