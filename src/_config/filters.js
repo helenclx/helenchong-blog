5d4d15e8943137f95ae37b27301c74103a11fc80
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-11-02 06:10:52 +0800

    Add spaces to Chinese date

--- src/_config/filters.js
+++ src/_config/filters.js
@@ -9,7 +9,7 @@ export default function(eleventyConfig) {
 
     // Format dates
     eleventyConfig.addFilter("formatDate", (date, pageLang="en") => {
-        const dateFormat = pageLang === "zh" ? "yyyy年L月d日" : "d LLLL yyyy";
+        const dateFormat = pageLang === "zh" ? "yyyy 年 L 月 d 日" : "d LLLL yyyy";
         if (typeof date === "object") {
             return DateTime.fromJSDate(date).setLocale(pageLang).toFormat(dateFormat);
         }
