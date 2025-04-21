606442bd40e0e92fd2e98418c84a572c6e22ee51
Helen Chong <119173961+helenclx@users.noreply.github.com>
2025-04-21 08:08:35 +0800

    Fix missing comma in file configuration

--- src/_config/files.js
+++ src/_config/files.js
@@ -1,9 +1,9 @@
-export default function(eleventyConfig) {
+export default function (eleventyConfig) {
     // Passthrough File Copy
     eleventyConfig.addPassthroughCopy("./src/assets/");
     eleventyConfig.addWatchTarget("./src/assets/");
     eleventyConfig.addPassthroughCopy({
-        "./src/assets/favicon/favicon.ico": "/favicon.ico"
+        "./src/assets/favicon/favicon.ico": "/favicon.ico",
         "./src/assets/favicon/apple-touch-icon.png": "/apple-touch-icon.png"
     });
 }
