40159a9a8a36c5d75703a71ee44abcf896daf4bc
Helen Chong <contact@helenchong.dev>
2025-04-27 19:10:46 +0800

    Refactor filter configurations

--- src/_config/filters.js
+++ src/_config/filters.js
@@ -1,18 +1,19 @@
 import { DateTime } from "luxon";
 import slug from 'limax';
 
-export default function(eleventyConfig) {
-    // Filter: Format dates
-    eleventyConfig.addFilter("formatDate", (date, pageLang="en") => {
-        const dateFormat = pageLang === "zh" ? "yyyy 年 L 月 d 日" : "d LLLL yyyy";
-        if (typeof date === "object") {
-            return DateTime.fromJSDate(date).setLocale(pageLang).toFormat(dateFormat);
-        }
-        return DateTime.fromISO(date, { setZone: true }).setLocale(pageLang).toFormat(dateFormat);
-    });
+// Filter: Format dates
+export const formatDate = (date, pageLang="en") => {
+    const dateFormat = pageLang === "zh" ? "yyyy 年 L 月 d 日" : "d LLLL yyyy";
+    if (typeof date === "object") {
+        return DateTime.fromJSDate(date).setLocale(pageLang).toFormat(dateFormat);
+    }
+    return DateTime.fromISO(date, { setZone: true }).setLocale(pageLang).toFormat(dateFormat);
+}
 
-    // Filter: URL slug generation with more language support tha slugify
-    eleventyConfig.addFilter("slug", (str) => {
-        return slug(str);
-    });
+// Filter: URL slug generation with more language support than slugify
+export const slugStr = (str) => slug(str);
+
+export default  {
+    formatDate,
+    slugStr,
 }
