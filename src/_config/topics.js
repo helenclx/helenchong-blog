a271a281c5b7884aaa490a48e326814eda10fa9e
Helen Chong <helenchong@omg.lol>
2026-07-18 18:43:42 +0800

    config/topics.js: use `const` keyword for variables that were never reassigned

--- src/_config/topics.js
+++ src/_config/topics.js
@@ -4,10 +4,10 @@ export default function (eleventyConfig) {
 	// Add post topic collection by tag
 	function addTopicCollection(tag, comparator) {
 		return (collectionApi) => {
-			let topics = new Set();
-			let posts = collectionApi.getFilteredByTag(tag);
+			const topics = new Set();
+			const posts = collectionApi.getFilteredByTag(tag);
 			posts.forEach((p) => {
-				let tops = p.data.topics;
+				const tops = p.data.topics;
 				if (tops) {
 					tops.forEach((c) => topics.add(c));
 				}
@@ -32,8 +32,8 @@ export default function (eleventyConfig) {
 
 	// Filter: Filter blog posts by topic
 	eleventyConfig.addFilter("filterByTopic", (posts, topic) => {
-		let result = posts.filter((p) => {
-			let tops = p.data.topics;
+		const result = posts.filter((p) => {
+			const tops = p.data.topics;
 			if (tops) {
 				return tops.includes(topic);
 			}
